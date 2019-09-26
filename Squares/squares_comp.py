ce023eead30dac4bef80c8609b0140e87a9ad1c2
TypeType <typetype@Mac-mini-TypeType.local>
2019-09-26 17:09:11 +0300

    Update squares_comp.py
    get 'ItalDelta' from master -> font -> input

--- Squares/squares_comp.py
+++ Squares/squares_comp.py
@@ -1,6 +1,6 @@
 #MenuTitle: Squares compensation
 # -*- coding: utf-8 -*-
-#Version: 0.1.3 (23 Sep, 2019)
+#Version: 0.1.2 (26 Sep, 2019)
 
 
 import math
@@ -21,18 +21,25 @@ def line_lenght(fnode, snode):
 	return math.sqrt((fnode.x - snode.x)**2 + (fnode.y - snode.y)**2)
 				
 
+def get_delta(thisLayer):
+	if thisLayer.master.customParameters['ItalDelta']:
+		delta_up, delta_down = [int(x) for x in re.findall('\-{0,1}\d+',
+		thisLayer.master.customParameters['ItalDelta'])]
+	elif thisLayer.master.font.customParameters['ItalDelta']:
+		delta_up, delta_down = [int(x) for x in re.findall('\-{0,1}\d+',
+		thisLayer.master.font.customParameters['ItalDelta'])]
+	else:
+		delta = AskString('Enter delta for compensation')
+		delta_up, delta_down = int(delta), int(delta)
+	return delta_up, delta_down
+
+
 def main():
 	Glyphs.clearLog()
 	thisFont = Glyphs.font
 	
-	if thisFont.customParameters['ItalDelta']:
-		delta_up, delta_down = [int(x) for x in re.findall('[0-9]+', thisFont.customParameters['ItalDelta'])]
-	else:
-		delta = AskString('Enter delta for compensation')
-		delta_up, delta_down = int(delta), int(delta)
-		if not delta:
-			return
 	for thisLayer in thisFont.selectedLayers:
+		delta_up, delta_down = get_delta(thisLayer)
 		lines = []
 		selectedNodes = []
 		for path in thisLayer.paths:
