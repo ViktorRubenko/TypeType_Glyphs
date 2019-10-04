3fb8decb7c9bb0924e099982eae5539d290ea682
TypeType <typetype@Mac-mini-TypeType.local>
2019-10-04 16:34:25 +0300

    Update formula_to_absolute.py

--- Formulas/formula_to_absolute.py
+++ Formulas/formula_to_absolute.py
@@ -1,26 +1,36 @@
 #MenuTitle: Formula to Absolute
 # -*- coding: utf-8 -*-
-#Version: 0.1.1 (04 Oct, 2019)
+#Version: 0.1.2 (04 Oct, 2019)
 
 Glyphs.clearLog()
 font = Glyphs.font
 selectedMasterIdx = font.masterIndex
 log = []
-for thisGlyph in font.glyphs:
+attribs = {
+	'leftMetricsKey': 'LSB',
+	'rightMetricsKey': 'RSB',
+	'widthMetricsKey': 'width'
+	}
+for thisGlyph in font.glyphs:	
 	absolute = []
 	thisLayer = thisGlyph.layers[selectedMasterIdx]
-	if thisLayer.leftMetricsKey and not thisGlyph.leftMetricsKey:
-		absolute.append('LSB: {}=>{}'.format(thisLayer.leftMetricsKey, thisLayer.LSB))
-		thisLayer.leftMetricsKey = None
-	if thisLayer.rightMetricsKey and not thisGlyph.rightMetricsKey:
-		absolute.append('RSB: {}=>{}'.format(thisLayer.rightMetricsKey, thisLayer.RSB))
-		thisLayer.rightMetricsKey = None
-	if thisLayer.widthMetricsKey and not thisGlyph.widthMetricsKey:
-		absolute.append('Width: {}=>{}'.format(thisLayer.widthMetricsKey, thisLayer.width))
-		thisLayer.widthMetricsKey = None
-	if absolute:
+	for attrib in ['leftMetricsKey', 'rightMetricsKey', 'widthMetricsKey']:
+		process = False
+		if sum([0 if not layer.__getattribute__(attrib) else 1
+			for layer in thisGlyph.layers]) == len(thisGlyph.layers):
+				thisGlyph.__setattr__(attrib, None)
+				process = True
+		if thisLayer.__getattribute__(attrib) and not thisGlyph.__getattribute__(attrib):
+			process = True
+		if process:
+			absolute.append('{}: {}=>{}'.format(attribs[attrib],
+				thisLayer.__getattribute__(attrib), 
+				thisLayer.__getattribute__(attribs[attrib]))
+				)
+			thisLayer.__setattr__(attrib, None)
+			log.append('{}: {}'.format(thisGlyph.name, '; '.join(absolute)))
 		thisLayer.syncMetrics()
-		log.append('{}: {}'.format(thisGlyph.name, '; '.join(absolute)))
+			
 if log:
 	Message('\n'.join(log))
 else:
