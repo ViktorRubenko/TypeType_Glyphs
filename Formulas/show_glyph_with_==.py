1d24b915674db3fc2a18b5cc2ded59985a73e362
TypeType <typetype@Mac-mini-TypeType.local>
2019-10-07 12:18:52 +0300

    Update show_glyph_with_==.py
    show all in one tab

--- Formulas/show_glyph_with_==.py
+++ Formulas/show_glyph_with_==.py
@@ -1,20 +1,21 @@
 #MenuTitle: Open glyphs with '=='
 # -*- coding: utf-8 -*-
-#Version: 0.1 (04 Oct, 2019)
+#Version: 0.1.1 (07 Oct, 2019)
 
 Glyphs.clearLog()
 font = Glyphs.font
 len_tabs = len(font.tabs)
+thisMasterId = font.masterIndex
+newTab= True
 for thisGlyph in font.glyphs:
-	newTab = True
-	for thisLayer in thisGlyph.layers:
-		if (thisLayer.leftMetricsKey or thisLayer.rightMetricsKey or
-			thisLayer.widthMetricsKey):
-			if newTab:
-				font.newTab()
-				newTab = False
-			font.tabs[-1].layers.append(thisLayer)
-			print(thisLayer)
+	thisLayer = thisGlyph.layers[thisMasterId]
+	if (thisLayer.leftMetricsKey or thisLayer.rightMetricsKey or
+		thisLayer.widthMetricsKey):
+		if newTab:
+			font.newTab()
+			newTab = False
+		font.tabs[-1].layers.append(thisLayer)
+		print(thisLayer)
 if len(font.tabs) == len_tabs:
 	Message('There are no individual formulas')
 print('Done')
