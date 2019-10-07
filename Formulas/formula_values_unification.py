75f56f27762d00b35b928a07fd85ec1ae3b3cc72
TypeType <typetype@Mac-mini-TypeType.local>
2019-10-07 17:17:54 +0300

    Update formula_values_unification.py

--- Formulas/formula_values_unification.py
+++ Formulas/formula_values_unification.py
@@ -1,86 +1,77 @@
 #MenuTitle: Formula values unification
 # -*- coding: utf-8 -*-
-#Version: 0.2.4 (07 Oct, 2019)
+#Version: 0.3.0 (07 Oct, 2019)
 
 
 import re
 
 
-def update_metrics(glyph):
-    for layer in glyph.layers:
-        layer.syncMetrics()
-
-def set_general_formula(thisGlyph, thisMasterIndex, attrib):
-    master_layer = thisGlyph.layers[thisMasterIndex]
-    if master_layer.__getattribute__(attrib):
-    	attrib_value = master_layer.__getattribute__(attrib)
-    else:
-    	attrib_value = thisGlyph.__getattribute__(attrib)
-    # clear formula in all masters
-    for thisLayer in thisGlyph.layers:
-        thisLayer.__setattr__(attrib, None)
-    # set general
-    thisGlyph.__setattr__(attrib, re.sub('[=]+', '=', attrib_value))
-
-def set_increment(thisGlyph, thisMasterIndex, attrib):
-    attribs = {
-        'rightMetricsKey': 'RSB',
-        'leftMetricsKey': 'LSB',
-        'widthMetricsKey': 'width'
-        }
-    try:
-        increment = int(re.findall(r'[+-]?\d+',
-                                   thisGlyph.__getattribute__(attrib))[0])
-    except IndexError:
-        increment = 0
-        
-    for thisLayerIndex, thisLayer in enumerate(thisGlyph.layers):
-        if thisLayerIndex == thisMasterIndex:
-            continue
-        delta = (thisGlyph.layers[thisMasterIndex].__getattribute__(attribs[attrib]) -
-                 thisLayer.__getattribute__(attribs[attrib]))
-        if not delta:
-            # No corrections needed
-            continue
-        delta = int(increment + delta)
-        if delta == 0:
-            thisLayer.__setattr__(
-                attrib, re.sub(r'[+-]?\d+', '',
-                               thisGlyph.__getattribute__(attrib)).replace('=', '==')
-                )
-        else:
-            if delta > 0:
-                delta = '+' + str(delta)
-            else:
-                delta = str(delta)
-            if re.search(r'[+-]?\d+', thisGlyph.__getattribute__(attrib)):
-                thisLayer.__setattr__(attrib,
-                                      re.sub(r'[+-]?\d+',
-                                             delta,
-                                             thisGlyph.__getattribute__(
-                                                 attrib)).replace('=', '=='))
-            else:
-                thisLayer.__setattr__(attrib,
-                    thisGlyph.__getattribute__(attrib) + delta
-                    )
+def update_metrics(thisGlyph):
+	for thisLayer in thisGlyph.layers:
+		thisLayer.syncMetrics()
+		
+def set_master_formula(thisGlyph, thisMasterIndex, attrib):
+	
+	attribs = {
+		'rightMetricsKey': 'RSB',
+		'leftMetricsKey': 'LSB',
+		'widthMetricsKey': 'width'
+		}
+	
+	master_formula = thisGlyph.layers[thisMasterIndex].__getattribute__(attrib)
+	glyph_formula = thisGlyph.__getattribute__(attrib)
+	if not master_formula:
+		if glyph_formula:
+			master_formula = glyph_formula
+		else:
+			return
+	master_value = thisGlyph.layers[thisMasterIndex].__getattribute__(attribs[attrib])
+	
+	try:
+		increment = int(re.findall(r'[+-]?\d+', master_formula)[0])
+	except IndexError:
+		increment = 0
+		
+	for thisLayerIndex, thisLayer in enumerate(thisGlyph.layers):
+		if thisLayerIndex == thisMasterIndex:
+			continue
+		oldValue = thisLayer.__getattribute__(attribs[attrib])
+		if glyph_formula == master_formula:
+			thisLayer.__setattr__(attrib, None)
+		else:
+			thisLayer.__setattr__(attrib, master_formula)
+		thisLayer.syncMetrics()
+		newValue = thisLayer.__getattribute__(attribs[attrib])
+		delta = oldValue - newValue
+		if delta:
+			delta = int(increment + delta)
+			if delta:
+				if delta >= 0:
+					delta = '+' + str(delta)
+				else:
+					delta = str(delta)
+				if increment:
+					thisLayer.__setattr__(attrib, re.sub(r'[+-]?\d+', 
+															delta, master_formula))
+				else:
+					thisLayer.__setattr__(attrib, master_formula + delta)
+			else:
+				thisLayer.__setattr__(attrib, re.sub(r'[+-]?\d+', '', master_formula))
+				
 
 def main():
-    Glyphs.clearLog()
-    font = Glyphs.font
-    thisMasterIndex = font.masterIndex
-    for thisGlyph in font.glyphs:
-    	if not thisGlyph.selected:
-    		continue
-        for attrib in ('leftMetricsKey', 'rightMetricsKey', 'widthMetricsKey'):
-            update_metrics(thisGlyph)
-            if (thisGlyph.layers[thisMasterIndex].__getattribute__(attrib) or
-            	thisGlyph.__getattribute__(attrib)):
-                set_general_formula(thisGlyph, thisMasterIndex, attrib)
-                update_metrics(thisGlyph)
-                set_increment(thisGlyph, thisMasterIndex, attrib)
-                update_metrics(thisGlyph)
-             
-
+	Glyphs.clearLog()
+	font = Glyphs.font
+	thisMasterIndex = font.masterIndex
+	for thisGlyph in font.glyphs:
+		if not thisGlyph.selected:
+			continue
+		for attrib in ('leftMetricsKey', 'rightMetricsKey', 'widthMetricsKey'):
+			update_metrics(thisGlyph)
+			set_master_formula(thisGlyph, thisMasterIndex, attrib)
+			update_metrics(thisGlyph)
+			
+	
+	
 if __name__ == '__main__':
-    main()
-
+	main()
