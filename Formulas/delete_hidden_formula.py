b6205fb4a1de210b28a519a2ec3166b3462facc3
TypeType <typetype@Mac-mini-TypeType.local>
2019-10-14 11:40:57 +0300

    Update delete_hidden_formula.py

--- Formulas/delete_hidden_formula.py
+++ Formulas/delete_hidden_formula.py
@@ -1,9 +1,16 @@
 #MenuTitle: Delete hidden formula
 # -*- coding: utf-8 -*-
-#Version: 0.0.1 (07 Oct, 2019)
+#Version: 0.0.2 (14 Oct, 2019)
 
 
 def main():
+	log = []
+	attribs = {
+				'leftMetricsKey': 'LeftSidebearing',
+				'rightMetricsKey': 'RightSidebearing',
+				'widthMetricsKey': 'Width'
+				}
+	Glyphs.clearLog()
 	font = Glyphs.font
 	for thisGlyph in font.glyphs:
 		for attrib in ('leftMetricsKey', 'rightMetricsKey', 'widthMetricsKey'):
@@ -11,8 +18,16 @@ def main():
 				continue
 			if (sum(0 if not thisLayer.__getattribute__(attrib) else 1 
 				for thisLayer in thisGlyph.layers) == len(thisGlyph.layers)):
-				print(thisGlyph.name, attrib, thisGlyph.__getattribute__(attrib))
+				log.append('{}: {} {}'.format(thisGlyph.name,
+											attribs[attrib],
+											thisGlyph.__getattribute__(attrib)))
 				thisGlyph.__setattr__(attrib, None)
+	print('Deleted formulas:')
+	if log:
+		print('\n'.join(log))
+	else:
+		print('Not found')
+	Glyphs.showMacroWindow()
 	
 	
 if __name__ == '__main__':
