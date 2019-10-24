b3e24bb6f49c0c40211c41fd2d5192902ba6f0de
TypeType <typetype@Mac-mini-TypeType.local>
2019-10-24 17:48:23 +0300

    Update overlap_path_metrics.py

--- Glyphs/overlap_path_metrics.py
+++ Glyphs/overlap_path_metrics.py
@@ -1,12 +1,12 @@
-#MenuTitle: Overlap->Path->Metrics
+#MenuTitle: Overlap-Path-Metrics
 # -*- coding: utf-8 -*-
-#Version: 0.1 (24 Oct, 2019)
+#Version: 0.2 (24 Oct, 2019)
 
 
 def overlap_path_metrics():
-	layer = Glyphs.font.selectedLayers[0] 
-	layer.correctPathDirection()
+	layer = Glyphs.font.selectedLayers[0]
 	layer.removeOverlap()
+	layer.correctPathDirection()
 	layer.syncMetrics()
 	
 	
