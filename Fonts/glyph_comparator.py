573bc12d201fa9114acd065b2bf2328f25af7fad
ViktorRubenko <vrubenko@gmail.com>
2020-03-19 11:45:13 +0300

    Check symmetric_difference between unicodes and names

--- Fonts/glyph_comparator.py
+++ Fonts/glyph_comparator.py
@@ -1,6 +1,6 @@
 # MenuTitle: Glyph Comparator
 # -*- coding: utf-8 -*-
-# Version: 0.0.1 (16 Mar, 2020)
+# Version: 0.0.2 (19 Mar, 2020)
 
 from vanilla import *
 
@@ -25,6 +25,16 @@ class ListWindow(object):
 if __name__ == "__main__":
     lw = ListWindow()
     font_2_names = tuple(glyph.name for glyph in FONT_2.glyphs)
+    font_1_names = tuple(glyph.name for glyph in FONT_1.glyphs)
+    print(set(font_1_names).symmetric_difference(set(font_2_names)))
+    for glyph_name in set(font_1_names).symmetric_difference(set(font_2_names)):
+        lw.w.myList.append(
+            {
+                "Type": "Existence by Name",
+                "GlyphName": glyph_name,
+                "Unicode": unicode_,
+            }
+        )
     for glyph in FONT_1.glyphs:
         if glyph.name in font_2_names:
             if glyph.unicode != FONT_2[glyph.name].unicode:
@@ -37,20 +47,22 @@ if __name__ == "__main__":
                         ),
                     }
                 )
-        else:
-            lw.w.myList.append(
-                {
-                    "Type": "Existance by GlyphName",
-                    "GlyphName": glyph.name,
-                    "Unicode": glyph.unicode,
-                }
-            )
     font_1_cmap = {
         glyph.unicode: glyph.name for glyph in FONT_1.glyphs if glyph.unicode
     }
     font_2_cmap = {
         glyph.unicode: glyph.name for glyph in FONT_2.glyphs if glyph.unicode
     }
+    for unicode_ in set(font_1_cmap).symmetric_difference(set(font_2_cmap)):
+        lw.w.myList.append(
+            {
+                "Type": "Existence by Unicode",
+                "GlyphName": font_1_cmap[unicode_]
+                if unicode_ in font_1_cmap
+                else font_2_cmap[unicode_],
+                "Unicode": unicode_,
+            }
+        )
     for unicode_, glyph_name in font_1_cmap.items():
         if unicode_ in font_2_cmap:
             if glyph_name != font_2_cmap[unicode_]:
@@ -63,11 +75,3 @@ if __name__ == "__main__":
                         "Unicode": unicode_,
                     }
                 )
-        else:
-            lw.w.myList.append(
-                {
-                    "Type": "Existance by Unicode",
-                    "GlyphName": glyph_name,
-                    "Unicode": unicode_,
-                }
-            )
