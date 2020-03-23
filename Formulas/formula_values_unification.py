646b7e44d3b7aad9408ae537fbfc7601a649191f
ViktorRubenko <vrubenko@gmail.com>
2020-03-23 13:21:21 +0300

    Fix for non-ascii layer names

--- Formulas/formula_values_unification.py
+++ Formulas/formula_values_unification.py
@@ -1,101 +1,117 @@
-#MenuTitle: Formula values unification
+# MenuTitle: Formula values unification
 # -*- coding: utf-8 -*-
-#Version: 0.3.3 (21 Oct, 2019)
+# Version: 0.3.4 (23 Mar, 2020)
 
 
 import re
 
 
 def update_metrics(thisGlyph):
-	for thisLayer in thisGlyph.layers:
-		thisLayer.syncMetrics()
-		
+    for thisLayer in thisGlyph.layers:
+        thisLayer.syncMetrics()
+
+
 def set_master_formula(thisGlyph, thisMasterIndex, attrib):
-	
-	log = []
-	
-	attribs = {
-		'rightMetricsKey': 'RSB',
-		'leftMetricsKey': 'LSB',
-		'widthMetricsKey': 'width'
-		}
-	
-	master_formula = thisGlyph.layers[thisMasterIndex].__getattribute__(attrib)
-	glyph_formula = thisGlyph.__getattribute__(attrib)
-	if not master_formula:
-		if glyph_formula:
-			master_formula = glyph_formula
-		else:
-			return
-	master_value = thisGlyph.layers[thisMasterIndex].__getattribute__(attribs[attrib])
-	
-	try:
-		increment = int(re.findall(r'[+-]?\d+', master_formula)[0])
-	except IndexError:
-		increment = 0
-		
-	for thisLayerIndex, thisLayer in enumerate(thisGlyph.layers):
-		
-		log.append('\t\t{}: {}({}) => '.format(thisLayer.name,
-								thisLayer.__getattribute__(attrib),
-								thisLayer.__getattribute__(attribs[attrib])))
-											
-		if thisLayerIndex == thisMasterIndex:
-			log[-1] = log[-1] + '{}({}) REF'.format(thisLayer.__getattribute__(attrib),
-												thisLayer.__getattribute__(attribs[attrib]))
-			continue
-		oldValue = thisLayer.__getattribute__(attribs[attrib])
-		if glyph_formula == master_formula:
-			thisLayer.__setattr__(attrib, None)
-		else:
-			thisLayer.__setattr__(attrib, master_formula)
-		thisLayer.syncMetrics()
-		newValue = thisLayer.__getattribute__(attribs[attrib])
-		delta = oldValue - newValue
-		if delta:
-			delta = int(increment + delta)
-			if delta:
-				if delta >= 0:
-					delta = '+' + str(delta)
-				else:
-					delta = str(delta)
-				if increment:
-					thisLayer.__setattr__(attrib, re.sub(r'[+-]?\d+', 
-															delta, master_formula))
-				else:
-					thisLayer.__setattr__(attrib, master_formula + delta)
-			else:
-				thisLayer.__setattr__(attrib, re.sub(r'[+-]?\d+', '', master_formula))
-			update_metrics(thisGlyph)
-			tLformula = thisLayer.__getattribute__(attrib)
-			if tLformula:
-				exception = re.findall(r'==(\d+[+-]\d+)\b',
-										thisLayer.__getattribute__(attrib))
-				if exception:
-					thisLayer.__setattr__(attrib, '=={}'.format(eval(exception[0])))
-			update_metrics(thisGlyph)
-		log[-1] = log[-1] + '{}({})'.format(thisLayer.__getattribute__(attrib),
-											thisLayer.__getattribute__(attribs[attrib]))
-					
-	print('\t' + attribs[attrib])
-	print('\n'.join(log))
-	print('\n')
-				
+
+    log = []
+
+    attribs = {
+        "rightMetricsKey": "RSB",
+        "leftMetricsKey": "LSB",
+        "widthMetricsKey": "width",
+    }
+
+    master_formula = thisGlyph.layers[thisMasterIndex].__getattribute__(attrib)
+    glyph_formula = thisGlyph.__getattribute__(attrib)
+    if not master_formula:
+        if glyph_formula:
+            master_formula = glyph_formula
+        else:
+            return
+    master_value = thisGlyph.layers[thisMasterIndex].__getattribute__(
+        attribs[attrib]
+    )
+
+    try:
+        increment = int(re.findall(r"[+-]?\d+", master_formula)[0])
+    except IndexError:
+        increment = 0
+
+    for thisLayerIndex, thisLayer in enumerate(thisGlyph.layers):
+
+        log.append(
+            "\t\t{}: {}({}) => ".format(
+                thisLayer.name.encode('utf-8').strip(),
+                thisLayer.__getattribute__(attrib),
+                thisLayer.__getattribute__(attribs[attrib]),
+            )
+        )
+
+        if thisLayerIndex == thisMasterIndex:
+            log[-1] = log[-1] + "{}({}) REF".format(
+                thisLayer.__getattribute__(attrib),
+                thisLayer.__getattribute__(attribs[attrib]),
+            )
+            continue
+        oldValue = thisLayer.__getattribute__(attribs[attrib])
+        if glyph_formula == master_formula:
+            thisLayer.__setattr__(attrib, None)
+        else:
+            thisLayer.__setattr__(attrib, master_formula)
+        thisLayer.syncMetrics()
+        newValue = thisLayer.__getattribute__(attribs[attrib])
+        delta = oldValue - newValue
+        if delta:
+            delta = int(increment + delta)
+            if delta:
+                if delta >= 0:
+                    delta = "+" + str(delta)
+                else:
+                    delta = str(delta)
+                if increment:
+                    thisLayer.__setattr__(
+                        attrib, re.sub(r"[+-]?\d+", delta, master_formula)
+                    )
+                else:
+                    thisLayer.__setattr__(attrib, master_formula + delta)
+            else:
+                thisLayer.__setattr__(
+                    attrib, re.sub(r"[+-]?\d+", "", master_formula)
+                )
+            update_metrics(thisGlyph)
+            tLformula = thisLayer.__getattribute__(attrib)
+            if tLformula:
+                exception = re.findall(
+                    r"==(\d+[+-]\d+)\b", thisLayer.__getattribute__(attrib)
+                )
+                if exception:
+                    thisLayer.__setattr__(
+                        attrib, "=={}".format(eval(exception[0]))
+                    )
+            update_metrics(thisGlyph)
+        log[-1] = log[-1] + "{}({})".format(
+            thisLayer.__getattribute__(attrib),
+            thisLayer.__getattribute__(attribs[attrib]),
+        )
+
+    print("\t" + attribs[attrib])
+    print("\n".join(log))
+    print("\n")
+
 
 def main():
-	Glyphs.clearLog()
-	font = Glyphs.font
-	thisMasterIndex = font.masterIndex
-	for thisGlyph in font.glyphs:
-		if not thisGlyph.selected:
-			continue
-		print('Glyph: ' + thisGlyph.name)
-		for attrib in ('leftMetricsKey', 'rightMetricsKey', 'widthMetricsKey'):
-			update_metrics(thisGlyph)
-			set_master_formula(thisGlyph, thisMasterIndex, attrib)
-		print('\n')
-			
-	
-	
-if __name__ == '__main__':
-	main()
+    Glyphs.clearLog()
+    font = Glyphs.font
+    thisMasterIndex = font.masterIndex
+    for thisGlyph in font.glyphs:
+        if not thisGlyph.selected:
+            continue
+        print("Glyph: " + thisGlyph.name)
+        for attrib in ("leftMetricsKey", "rightMetricsKey", "widthMetricsKey"):
+            update_metrics(thisGlyph)
+            set_master_formula(thisGlyph, thisMasterIndex, attrib)
+        print("\n")
+
+
+if __name__ == "__main__":
+    main()
--- Formulas/show_glyphs_with_different_formulas_logic.py
+++ Formulas/show_glyphs_with_different_formulas_logic.py
@@ -1,36 +1,43 @@
-#MenuTitle: Show glyphs with different formula logic
+# MenuTitle: Show glyphs with different formula logic
 # -*- coding: utf-8 -*-
-#Version: 0.1.3 (07 Oct, 2019)
+# Version: 0.1.4 (23 Mar, 2020)
 
 
 import re
 
 
 def get_key(pattern, f_key):
-	try:
-		return re.findall(pattern, f_key)[0]
-	except IndexError:
-		return ''
-		
+    try:
+        return re.findall(pattern, f_key)[0]
+    except IndexError:
+        return ""
+
+
 def add_to_log(thisGlyph, log, attrib):
-	keys = {'leftMetricsKey': 'Left Sidebearing', 
-			'rightMetricsKey': 'Right Sidebearing', 
-			'widthMetricsKey': 'Width'}
-	for thisLayer in thisGlyph.layers:
-		attrib_log = log.setdefault(keys[attrib], {})
-		glyph_log = attrib_log.setdefault(thisGlyph.name, [])
-		glyph_log.append('{}: {}'.format(
-			thisLayer.name, thisLayer.__getattribute__(attrib)))
-			
+    keys = {
+        "leftMetricsKey": "Left Sidebearing",
+        "rightMetricsKey": "Right Sidebearing",
+        "widthMetricsKey": "Width",
+    }
+    for thisLayer in thisGlyph.layers:
+        attrib_log = log.setdefault(keys[attrib], {})
+        glyph_log = attrib_log.setdefault(thisGlyph.name, [])
+        glyph_log.append(
+            "{}: {}".format(
+                thisLayer.name.encode("utf-8").strip(),
+                thisLayer.__getattribute__(attrib),
+            )
+        )
+
+
 def print_log(log):
-	for param_name, glyph_data in log.items():
-		print('{}\n'.format(param_name))
-		for glyph_name, glyph_layers in glyph_data.items():
-			print(glyph_name)
-			for layer in glyph_layers:
-				print('\t{}'.format(layer))
-		print('\n')		
-		
+    for param_name, glyph_data in log.items():
+        print("{}\n".format(param_name))
+        for glyph_name, glyph_layers in glyph_data.items():
+            print(glyph_name)
+            for layer in glyph_layers:
+                print("\t{}".format(layer))
+        print("\n")
 
 
 Glyphs.clearLog()
@@ -38,61 +45,65 @@ font = Glyphs.font
 len_tabs = len(font.tabs)
 log = {}
 for thisGlyph in font.glyphs:
-	#newTab = True
-	passed = False
-	for attrib in ['leftMetricsKey', 'rightMetricsKey', 'widthMetricsKey']:
-		if (sum(0 if not layer.__getattribute__(attrib) else 1 
-			for layer in thisGlyph.layers) != len(thisGlyph.layers)):
-				for thisLayer in thisGlyph.layers:
-					if thisLayer.__getattribute__(attrib):
-						add_to_log(thisGlyph, log, attrib)
-						passed = True
-						break
-				if passed:
-					break
-		if passed:
-			break
-		
-		glyph_key = thisGlyph.__getattribute__(attrib)
-		if glyph_key:
-			main_basis = get_key(r'={0,2}([A-z\|\.]+)', glyph_key)
-			for thisLayer in thisGlyph.layers:
-				layer_key = thisLayer.__getattribute__(attrib)
-				if not layer_key:
-					continue
-				layer_basis = get_key(r'={0,2}([A-z\|\.]+)', layer_key)
-				if layer_basis != main_basis:
-					add_to_log(thisGlyph, log, attrib)
-					break
-					'''
+    # newTab = True
+    passed = False
+    for attrib in ["leftMetricsKey", "rightMetricsKey", "widthMetricsKey"]:
+        if sum(
+            0 if not layer.__getattribute__(attrib) else 1
+            for layer in thisGlyph.layers
+        ) != len(thisGlyph.layers):
+            for thisLayer in thisGlyph.layers:
+                if thisLayer.__getattribute__(attrib):
+                    add_to_log(thisGlyph, log, attrib)
+                    passed = True
+                    break
+            if passed:
+                break
+        if passed:
+            break
+
+        glyph_key = thisGlyph.__getattribute__(attrib)
+        if glyph_key:
+            main_basis = get_key(r"={0,2}([A-z\|\.]+)", glyph_key)
+            for thisLayer in thisGlyph.layers:
+                layer_key = thisLayer.__getattribute__(attrib)
+                if not layer_key:
+                    continue
+                layer_basis = get_key(r"={0,2}([A-z\|\.]+)", layer_key)
+                if layer_basis != main_basis:
+                    add_to_log(thisGlyph, log, attrib)
+                    break
+                    """
 					if newTab:
 						font.newTab()
 						newTab = False
 					font.tabs[-1].layers.append(thisLayer)
-					'''
-		else:
-			main_basis = None
-			basisIndex = None
-			for thisLayerIndex, thisLayer in enumerate(thisGlyph.layers):
-				if thisLayer.__getattribute__(attrib):
-					if not main_basis:
-						main_basis = get_key(r'={0,2}([A-z\|\.]+)',
-						thisLayer.__getattribute__(attrib))
-						basisIndex = thisLayerIndex 
-						continue
-					layer_key = thisLayer.__getattribute__(attrib)
-					layer_basis = get_key(r'={0,2}([A-z\|\.]+)', layer_key)
-					if layer_basis != main_basis:
-						add_to_log(thisGlyph, log, attrib)
-						break
-						'''
+					"""
+        else:
+            main_basis = None
+            basisIndex = None
+            for thisLayerIndex, thisLayer in enumerate(thisGlyph.layers):
+                if thisLayer.__getattribute__(attrib):
+                    if not main_basis:
+                        main_basis = get_key(
+                            r"={0,2}([A-z\|\.]+)",
+                            thisLayer.__getattribute__(attrib),
+                        )
+                        basisIndex = thisLayerIndex
+                        continue
+                    layer_key = thisLayer.__getattribute__(attrib)
+                    layer_basis = get_key(r"={0,2}([A-z\|\.]+)", layer_key)
+                    if layer_basis != main_basis:
+                        add_to_log(thisGlyph, log, attrib)
+                        break
+                        """
 						if newTab:
 							font.newTab()
 							newTab = False
 						font.tabs[-1].layers.append(thisLayer)
-						'''
+						"""
 if log:
-	Glyphs.showMacroWindow()
-	print_log(log)
+    Glyphs.showMacroWindow()
+    print_log(log)
 else:
-	Message('There are no formulas with the wrong logic')
+    Message("There are no formulas with the wrong logic")
