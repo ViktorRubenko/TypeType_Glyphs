cf7f3938e37c709cd8936e631eee8785e83df02e
Viktor Rubenko <vrubenko@gmail.com>
2021-06-15 13:59:29 +0300

    add __doc__

--- Contours/decompose_backbround_rmoverlap.py
+++ Contours/decompose_backbround_rmoverlap.py
@@ -1,6 +1,11 @@
 # MenuTitle: Decompose - Background - RM Overlap
 # -*- coding: utf-8 -*-
 
+__doc__ = """
+    1. Decompose components
+    2. Place new contour to background
+    3. Remove overlaps
+"""
 
 font = Glyphs.font
 
